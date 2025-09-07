715201358adf292bd19ca041ecaf763f304738a7
Ekram <ekrunur84@gmail.com>
2025-09-07 22:10:30 +0300

    h

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.views.generic import DetailView
 from django.contrib.auth import login
 from django.contrib.auth import authenticate, logout
-from django.contrib.auth.forms import  UserCreationForm
+from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth.decorators import login_required, user_passes_test, permission_required  # Ensure this line is present
 from .models import Book, Library, UserProfile
