eb8e7be8d370ad61395d948f16095a4680730952
Ekram <ekrunur84@gmail.com>
2025-09-07 21:57:43 +0300

    h

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,13 +1,14 @@
 from django.urls import path
-from django.views.generic import RedirectView
+from django.contrib.auth import views as auth_views
+from django.views.generic.base import RedirectView  # <-- Add this import
 from . import views
 
 urlpatterns = [
     path('', RedirectView.as_view(url='books/', permanent=False)),
     path('books/', views.book_list, name='book_list'),
-    path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
-    path('login/', views.user_login, name='login'),
-    path('logout/', views.user_logout, name='logout'),
+    path('library/detail/<int:id>/', views.LibraryDetailView.as_view(), name='library_detail'),
+    path('login/', auth_views.LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
     path('register/', views.register, name='register'),
     path('admin/', views.admin_view, name='admin_view'),
     path('librarian/', views.librarian_view, name='librarian_view'),
