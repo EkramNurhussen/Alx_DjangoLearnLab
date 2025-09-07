ace0912bcfd73f23eef0e4b426910b2307e87329
Ekram <ekrunur84@gmail.com>
2025-09-07 17:07:00 +0300

    Task 1: Added Library import, implemented class-based view with DetailView, updated URLs, and ensured templates

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -8,7 +8,7 @@ class Author(models.Model):
 class Book(models.Model):
     title = models.CharField(max_length=100)
     author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='books')
-    publication_year = models.IntegerField()
+    publication_year = models.IntegerField(default=2023)
     def __str__(self):
         return self.title
 
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import DetailView  # Using generic import for clarity
 from .models import Book, Library  # Added Library import
-
+from django.views.generic.detail import DetailView
 def book_list(request):
     books = Book.objects.all()
     return render(request, 'relationship_app/list_books.html', {'books': books})
