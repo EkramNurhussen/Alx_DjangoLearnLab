36f703a30a7ab2db26a1c8be80b61aa2243ddac5
Ekram <ekrunur84@gmail.com>
2025-10-08 11:57:05 +0300

    task0

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,8 +1,12 @@
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.shortcuts import render, redirect
 from django.contrib.auth import login
 from django.contrib.auth.decorators import login_required
-from .forms import CustomUserCreationForm, UserProfileForm
+from .models import Post
+from .forms import CustomUserCreationForm, UserProfileForm, PostForm
 
+# Authentication views (from Task 1)
 def register(request):
     if request.method == 'POST':
         form = CustomUserCreationForm(request.POST)
@@ -24,3 +28,38 @@ def profile(request):
     else:
         form = UserProfileForm(instance=request.user)
     return render(request, 'blog/profile.html', {'form': form})
+
+# Post views
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/post_list.html'
+    context_object_name = 'posts'
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'blog/post_detail.html'
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    form_class = PostForm
+    template_name = 'blog/post_form.html'
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post
+    form_class = PostForm
+    template_name = 'blog/post_form.html'
+
+    def test_func(self):
+        return self.get_object().author == self.request.user
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
+    template_name = 'blog/post_confirm_delete.html'
+    success_url = '/'
+
+    def test_func(self):
+        return self.get_object().author == self.request.user
