0fa177947b0041af1886d04c370365134b8a8ff7
Ekram <ekrunur84@gmail.com>
2025-10-08 14:40:39 +0300

    test

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -2,7 +2,7 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from .models import Post, Comment
-from taggit.forms import TagField
+from taggit.forms import TagWidget
 
 class CustomUserCreationForm(UserCreationForm):
     email = forms.EmailField(required=True)
@@ -17,11 +17,12 @@ class UserProfileForm(forms.ModelForm):
         fields = ['username', 'email']
 
 class PostForm(forms.ModelForm):
-    tags = TagField(required=False)
-
     class Meta:
         model = Post
         fields = ['title', 'content', 'tags']
+        widgets = {
+            'tags': TagWidget()
+        }
 
 class CommentForm(forms.ModelForm):
     class Meta:
--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -12,6 +12,10 @@ class Post(models.Model):
     def __str__(self):
         return self.title
 
+    def get_absolute_url(self):
+        from django.urls import reverse
+        return reverse('post_detail', kwargs={'pk': self.pk})
+
 class Comment(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
     author = models.ForeignKey(User, on_delete=models.CASCADE)
