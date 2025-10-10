9b1a97ddcf003c9b0789d83f87204f7990ab5390
Ekram <ekrunur84@gmail.com>
2025-10-10 11:06:39 +0300

    only accounts

--- social_media_api/accounts/urls.py
+++ social_media_api/accounts/urls.py
@@ -6,7 +6,6 @@ urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
     path('login/', LoginView.as_view(), name='login'),
     path('profile/', ProfileView.as_view(), name='profile'),
-    urlpatterns += [
-    path('follow/<int:pk>/', FollowViewSet.as_view({'post': 'follow'}), name='follow'),
-    path('unfollow/<int:pk>/', FollowViewSet.as_view({'post': 'unfollow'}), name='unfollow'),
+    path('follow/<int:user_id>/', FollowViewSet.as_view({'post': 'follow'}), name='follow'),
+    path('unfollow/<int:user_id>/', FollowViewSet.as_view({'post': 'unfollow'}), name='unfollow'),
 ]
