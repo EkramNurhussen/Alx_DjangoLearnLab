fb5e56873fb0c43183ce64de0f543211f76f46e7
Ekram <ekrunur84@gmail.com>
2025-10-10 10:33:06 +0300

    only accounts

--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -1,33 +1,3 @@
-from rest_framework import status
-from rest_framework.decorators import api_view, permission_classes
-from rest_framework.permissions import IsAuthenticated
-from rest_framework.response import Response
-from rest_framework.authtoken.models import Token
-from django.contrib.auth import authenticate
-from .serializers import RegisterSerializer, LoginSerializer, UserSerializer
+from django.shortcuts import render
 
-@api_view(['POST'])
-def register(request):
-    serializer = RegisterSerializer(data=request.data)
-    if serializer.is_valid():
-        user = serializer.save()
-        token = Token.objects.get(user=user)
-        return Response({'token': token.key, 'user': UserSerializer(user).data}, status=status.HTTP_201_CREATED)
-    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-@api_view(['POST'])
-def login(request):
-    serializer = LoginSerializer(data=request.data)
-    if serializer.is_valid():
-        user = authenticate(username=serializer.validated_data['username'], password=serializer.validated_data['password'])
-        if user:
-            token = Token.objects.get(user=user)
-            return Response({'token': token.key, 'user': UserSerializer(user).data}, status=status.HTTP_200_OK)
-        return Response({'error': 'Invalid credentials'}, status=status.HTTP_401_UNAUTHORIZED)
-    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-@api_view(['GET'])
-@permission_classes([IsAuthenticated])
-def profile(request):
-    serializer = UserSerializer(request.user)
-    return Response(serializer.data)
+# Create your views here.
